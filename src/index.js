4da735e90675afe5f0790401b4fe7a416f89a829
William Silva <williaamsf@gmail.com>
2022-09-27 00:46:25 -0300

    cria deposito

--- src/index.js
+++ src/index.js
@@ -43,8 +43,22 @@ app.post('/account', (request, response) => {
 
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
-    
+
     return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    };
+    
+    customer.statement.push(statementOperation);
+    return response.status(201).json({ message: 'Successful deposit' });
 });
 
 app.listen(3333);
